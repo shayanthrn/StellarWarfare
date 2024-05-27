7b668d625f464d511da08b299fbb78c1530c5203
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2024-05-27 00:52:25 -0400

    add git ignore and project files

---
+++ Source/StellarWarfare/StellarWarfare.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StellarWarfare.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StellarWarfare, "StellarWarfare" );
